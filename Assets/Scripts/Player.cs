cc1e8f7d1923b3baa6494858b053ce9489512f31
Alex N <ichiv.0zv@gmail.com>
2024-01-27 23:03:23 +0000

    VFX test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,8 @@ public class Player : MonoBehaviour
     [SerializeField, Min(0)] float bodyTurnSpeed;
     [SerializeField, Range(0,90)] float headTurnBounds;
 
+    public GameObject impactPrefab;
+
     bool canJump = false;
     float jumpForce = 0;
 
@@ -47,6 +49,11 @@ public class Player : MonoBehaviour
             ScoreItems scoreItems = interactable.GetComponent<ScoreItems>();
             if (scoreItems != null)
             {
+                Vector3 collisionPoint = interactable.ClosestPoint(transform.position);
+
+                GameObject effect = Instantiate(impactPrefab, collisionPoint, Quaternion.identity);
+                
+                Destroy(effect, 3f);
                 switch (scoreItems.objectsType)
                 {
                     case ScoreItems.ObjectsType.Dynamic:
